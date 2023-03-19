f17fa3c6e124f110554e4acd40ff0c22f8838fdd
Sourav halder <souravh093@gmail.com>
2023-03-19 23:57:26 +0600

    some updtat

--- js/countries.js
+++ js/countries.js
@@ -10,14 +10,37 @@ const displayCountries = data => {
     container.innerHTML = allCountriesHTML.join(' '); 
 }
 
-const getCountriesHTML = country => {
+
+// option 2
+const getCountriesHTML = ({name, flags}) => {
     return `
-        <div>
-            <h2>${country.name.common}</h2>
-            <img src="${country.flags.png}" alt="country-flags">
+        <div class="country">
+            <h2>${name.common}</h2>
+            <img src="${flags.png}" alt="country-flags">
         </div>
     `
 }
+
+// original default
+// const getCountriesHTML = country => {
+//     return `
+//         <div class="country">
+//             <h2>${country.name.common}</h2>
+//             <img src="${country.flags.png}" alt="country-flags">
+//         </div>
+//     `
+// }
+// option 1
+// const getCountriesHTML = country => {
+//     // option one
+//     const {name, flags} = country;
+//     return `
+//         <div class="country">
+//             <h2>${name.common}</h2>
+//             <img src="${flags.png}" alt="country-flags">
+//         </div>
+//     `
+// }
 
 
 
